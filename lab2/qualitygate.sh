561c9fbf863b67d02762df8f8f098538545ad779
Jeff Yarbrough <pcjeffmac@gmail.com>
2020-01-23 14:11:53 -0500

    Change for script path.

--- lab2/qualitygate.sh
+++ lab2/qualitygate.sh
@@ -63,7 +63,7 @@ execute_query() {
 
 
 # validate have Dynatrace credentials
-CREDS=/home/ubuntu/scripts/script-inputs.json
+CREDS=/home/dtu_training/scripts/script-inputs.json
 
 if ! [ -f "$CREDS" ]; then
   echo "Aborting: Missing $CREDS file"
